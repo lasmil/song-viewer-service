490775960c43317b3a2795115f3fb3f2a878fc91
Laszlo Mile <mlacif1@gmail.com>
2022-11-15 14:13:44 +0100

    Added vercel config
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
 import Fastify from 'fastify'
-import pino from 'pino'
 
 import analyze from './routes/analyze.js'
-
-const transport = pino.transport({
-  target: 'pino-pretty',
-  options: { colorize: true }
-})
-
-const logger = pino(
-  {
-    level: 'info',
-    base: null,
-    timestamp: false
-  },
-  transport
-)
 
 const fastify = Fastify({
   logger: true
@@ -25,7 +10,7 @@ fastify.register(analyze)
 
 fastify.listen({ port: 3001 }, function (err, address) {
   if (err) {
-    logger.error(err)
+    fastify.log.error(err)
   }
-  logger.info(`Server listening on ${address}`)
+  fastify.log.info(`Server listening on ${address}`)
 })
